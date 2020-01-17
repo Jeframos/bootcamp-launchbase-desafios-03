25014b0175b29d16cadf9cc2765b6d8d04641b0b
Jeffeson Ramos <jefferson.ramos.ext@sodexo.com>
2020-01-17 00:46:33 -0300

    Update

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const recipes = require("./data")
 
 const server = express()
 
-
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res){
-    return res.render("index")
+let recipesFiltered = []
+
+for(let i = 0; i < 6; i++){
+    recipesFiltered.push(recipes[i])
+}
+
+    console.log(recipesFiltered.length)
+
+    return res.render("home", {items: recipesFiltered})
+})
+
+server.get("/recipes", function(req, res){
+    return res.render("recipes", {items: recipes})
+})
+
+server.get("/recipes/:index", function(req, res){
+    const { index: recipeIndex } = req.params
+
+    const recipe = recipes[recipeIndex]
+
+    if(!recipe) return res.send("Recipe not found!")   
+    return res.render("recipe", { item: recipe})
 })
 
 server.get("/about", function(req, res){
     return res.render("about")
-})
-
-server.get("/receitas", function(req, res){
-    return res.render("receitas")
 })
 
 server.listen(3000, function(){
